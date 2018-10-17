4c398b85666aeb9570fe6524d7590983a7b75b7f
gabrielarafael <42839556+gabrielarafael@users.noreply.github.com>
2018-10-17 11:27:23 -0300

    Feita questao_2

--- questoes/questao_2.py
+++ questoes/questao_2.py
@@ -40,23 +40,23 @@
 def main():
 	x = y = 0 #espaço inicial x = 0 e y = 0
     while True:
-    	comando = input("Digite comando: ") #recebe comando do teclado
-    	if len(comando) == 0: #se o comando for vazio, encerra loop
+    	comando = input("Digite comando: ") #vai receber o comando do teclado
+    	if len(comando) == 0: #se o comando estiver vazio, acaba o loop
     		break
-    	direcao = comando[:comando.index(" ")] #direcao é a substring do começo ate o espaço
+    	direcao = comando[:comando.index(" ")] #a direcao é substring do começo até o espaço
     	if comando[comando.index(" ")+1:].isdigit(): #se a partir do espaço, o que sobra é um digito, atribui a q
     		qtd = int(comando[comando.index(" ")+1:])
-    	else: #se a partir do espaço há um input invalido, a quantidade é invalida e a entrada é descartada
+    	else: #se a partir do espaço há um input invalido, a qtdade dá invalida e a entrada é descartada
     		continue
-    	if direcao == "CIMA": #se foi pra cima, aumenta a coordenada y
+    	if direcao == "CIMA": #foi pra cima, aumenta y
     		y += qtd
-    	elif direcao == "BAIXO": #se foi pra baixo, diminui a coordenada y
+    	elif direcao == "BAIXO": #foi pra baixo, diminui y
     		y -= qtd
-    	elif direcao == "ESQUERDA": #se foi pra esquerda, diminui a coordenada x
+    	elif direcao == "ESQUERDA": #foi pra esquerda, diminui x
     		x -= qtd
-    	elif direcao == "DIREITA": #se foi pra direita, aumenta a coordenada x
+    	elif direcao == "DIREITA": #foi pra direita, aumenta x
     		x += qtd
-    	else: #direcao invalida, desconsidera a entrada
+    	else: #direcao inválida, desconsidera a entrada
     		continue
     print(round((x**2 + y**2)**.5)) #imprime na tela a distancia como o inteiro mais proximo do resultado
 
